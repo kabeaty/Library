c7d8660a9b958b27e487e9ba253f8eec94b809cc
Kathryn Beaty <kabeaty@gmail.com>
2014-02-05 14:37:15 -0600

    Add setter methods for class Recipe

--- cookbook.rb
+++ cookbook.rb
@@ -34,4 +34,28 @@ class Recipe
     @steps
   end
 
+  def title=(new_title)
+    @title = new_title
+  end
+
+  def ingredients=(new_ingredients)
+    @ingredients = new_ingredients
+  end
+
+  def steps=(new_steps)
+    @steps = new_steps
+  end
+
 end
+
+
+
+
+
+
+
+
+
+
+
+
