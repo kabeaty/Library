d48f0bdc90a05322ba794b2aeabae6b233bcbdb5
Kathryn Beaty <kabeaty@gmail.com>
2014-02-05 14:42:37 -0600

    Refactor getter and setter methods for Cookbook and Recipe classes with attr_accessor

--- cookbook.rb
+++ cookbook.rb
@@ -1,49 +1,21 @@
 class Cookbook
+  attr_accessor :title
 
   def initialize(title)
     @title = title
   end
 
-  def title
-    @title
-  end
-
-  def title=(new_title)
-    @title = new_title
-  end
-
 end
 
 class Recipe
+  attr_accessor :title
+  attr_accessor :ingredients
+  attr_accessor :steps
 
   def initialize(title, ingredients, steps)
     @title = title
     @ingredients = ingredients
     @steps = steps
-  end
-
-  def title
-    @title
-  end
-
-  def ingredients
-    @ingredients
-  end
-
-  def steps
-    @steps
-  end
-
-  def title=(new_title)
-    @title = new_title
-  end
-
-  def ingredients=(new_ingredients)
-    @ingredients = new_ingredients
-  end
-
-  def steps=(new_steps)
-    @steps = new_steps
   end
 
 end
